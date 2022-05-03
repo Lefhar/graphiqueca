8d518bbb6cc1f2ebb555647a7d9292516a04c333
Lefhar <s.lefebvre907@laposte.net>
2022-05-03 14:15:47 +0200

    changement de nom du projet

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>teste chart</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
 </head>
@@ -12,13 +13,13 @@
 <div class="row">
     <div class="container">
         <div class="col-md-4">
-            <canvas id="vertical" ></canvas>
+            <canvas id="vertical"></canvas>
         </div>
         <div class="col-md-4">
-            <canvas id="horizontal" ></canvas>
+            <canvas id="horizontal"></canvas>
         </div>
         <div class="col-md-4">
-            <canvas id="gauge" ></canvas>
+            <canvas id="gauge"></canvas>
         </div>
 
 
@@ -33,57 +34,57 @@
         data: {
             datasets: [{
                 label: 'CA du mois',
-                data: [6000, 5900, 8000, 8100,6000, 5900, 8000, 8100,7600, 4509, 8120, 8441],
+                data: [6000, 5900, 8000, 8100, 6000, 5900, 8000, 8100, 7600, 4509, 8120, 8441],
                 backgroundColor: '#5b9bd5',
                 // this dataset is drawn below
                 order: 2
             }, {
                 label: 'Moyenne CA',
-                data: [600, 590, 800, 810,600, 590, 800, 810,600, 1590, 800, 810],
+                data: [600, 590, 800, 810, 600, 590, 800, 810, 600, 1590, 800, 810],
                 backgroundColor: '#ed7d31',
-                         borderColor: '#ed7d31',
+                borderColor: '#ed7d31',
                 type: 'line',
                 // this dataset is drawn on top
                 order: 1
             }],
-            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout','Septembre','Octobre','Novembre','Décembre']
+            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
         },
         options: {}
     });
-const mixedChart2 = new Chart(ctx2, {
+    const mixedChart2 = new Chart(ctx2, {
         type: 'bar',
         data: {
             datasets: [{
                 label: 'CA du mois',
-                data: [6000, 5900, 8000, 8100,6000, 5900, 8000, 8100,7600, 4509, 8120, 8441],
+                data: [6000, 5900, 8000, 8100, 6000, 5900, 8000, 8100, 7600, 4509, 8120, 8441],
                 backgroundColor: '#5b9bd5',
                 // this dataset is drawn below
                 order: 2
             }, {
                 label: 'Moyenne CA',
-                data: [600, 590, 800, 810,600, 590, 800, 810,600, 1590, 800, 810],
+                data: [600, 590, 800, 810, 600, 590, 800, 810, 600, 1590, 800, 810],
                 backgroundColor: '#ed7d31',
-                         borderColor: '#ed7d31',
+                borderColor: '#ed7d31',
                 type: 'line',
                 // this dataset is drawn on top
                 order: 1
             }],
-            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout','Septembre','Octobre','Novembre','Décembre']
+            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
         },
         options: {indexAxis: 'y'}
     });
 
-const mixedChart3 = new Chart(ctx3, {
+    const mixedChart3 = new Chart(ctx3, {
         type: 'doughnut',
         data: {
             datasets: [{
                 label: 'CA du mois',
-                data: [6000, 5900, 8000, 8100,6000, 5900, 8000, 8100,7600, 4509, 8120, 8441],
+                data: [6000, 5900, 8000, 8100, 6000, 5900, 8000, 8100, 7600, 4509, 8120, 8441],
                 backgroundColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
                 // this dataset is drawn below
 
             }],
-            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout','Septembre','Octobre','Novembre','Décembre']
+            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
         },
         options: {
             rotation: 270,
@@ -111,7 +112,9 @@ const mixedChart3 = new Chart(ctx3, {
     //
     // new Chart(ctx3, options);
 </script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
 </body>
 </html>
